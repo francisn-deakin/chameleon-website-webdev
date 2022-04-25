959dbfe879ed3cf30ad29c4bfb26201aebcf4b24
Shakwat Limon <shlimon@deakin.edu.au>
2022-04-25 18:58:22 +1000

    Another Commit

--- client/src/components/Portfolio.vue
+++ client/src/components/Portfolio.vue
@@ -6,7 +6,7 @@
     >
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">
-          Chameleon Website Portfolio Page
+          Chameleon Website Portfolio Page. Great Porfolios
         </h1>
         <p class="subheading font-weight-regular pt-2">
           contents
